0040ef4193ddf5e47d6767f16429ca8b701b2576
inesmoreira <simoreira@ua.pt>
2017-12-11 21:11:10 +0000

    Connection to the database

--- musicbox/views.py
+++ musicbox/views.py
@@ -4,41 +4,32 @@ from urllib.request import urlopen
 import xml.dom.minidom
 import os
 from django.http import HttpRequest
-
-
-def parse_from_api(url, file_name):
-    s = urlopen(url)
-    contents = s.read()
-    file = open("%s/%s.xml" % (os.path.dirname(os.path.abspath(__file__)) ,file_name), 'wb')
-    file.write(contents)
-    file.close()
-    doc = xml.dom.minidom.parse("%s/%s.xml" % (os.path.dirname(os.path.abspath(__file__)), file_name))
-    content = doc.toxml()
-
-    os.remove("%s/%s.xml" % (os.path.dirname(os.path.abspath(__file__)), file_name))
+import json
+from s4api.graphdb_api import GraphDBApi
+from s4api.swagger import ApiClient
 
 def database():
-    try:
-        pass
-    except:
-        print("Creating database")
-        #create database
+        endpoint = "http://localhost:7200"
+        repo_name = "musicbox"
+        client = ApiClient(endpoint=endpoint)
+        accessor = GraphDBApi(client)
 
-        #seed database
-        doc = xml.dom.minidom.parse("%s/artists.xml" % os.path.dirname(os.path.abspath(__file__)))
-        content = doc.toxml()
+        query = """
+                PREFIX foaf:<http://xmlns.com/foaf/spec/>
+                PREFIX artist:<http://www.artists.com/>
+                SELECT ?namealbum
+                WHERE{
+                    ?b artist:album ?a.
+                    ?a foaf:name_album ?namealbum.
+                }
+                """
 
-        doc = xml.dom.minidom.parse("%s/Users.xml" % os.path.dirname(os.path.abspath(__file__)))
-        content = doc.toxml()
-
-        #add xml with top current tracks
-        get_top_tracks_url = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=79004d202567282ea27ce27e9c26a498"
-        parse_from_api(get_top_tracks_url, "toptracks")
-
-        #add xml with top portugal tracks
-        get_pt_top_tracks_url = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=portugal&api_key=79004d202567282ea27ce27e9c26a498"
-        parse_from_api(get_pt_top_tracks_url, "toptrack_portugal")
-
+        payload_query = {"query": query}
+        res = accessor.sparql_select(body=payload_query,
+                                     repo_name=repo_name)
+        res = json.loads(res)
+        for e in res['results']['bindings']:
+            print(e['namealbum']['value'])
 
 def home(request):
     database()
@@ -48,26 +39,28 @@ def home(request):
 
 def search_query(request):
     pass
+
+
 def artists(request):
     pass
 
+
 def albums(request):
     pass
+
 
 def albuminfo(request):
     assert isinstance(request, HttpRequest)
     pass
 
+
 def artist_page(request):
     pass
+
+
 def charts(request):
     pass
 
-def login(request):
-    return render(request, 'logIn.html')
-
-def register(request):
-    return render(request, 'register.html')
 
 def profile(request):
     pass
